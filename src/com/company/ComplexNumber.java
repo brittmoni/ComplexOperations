8437194b1edecac8bac427b43c368ab26f759fcf
Brittany F <brittfranklin92@gmail.com>
2021-11-27 19:54:50 -0500

    Adding methods

--- src/com/company/ComplexNumber.java
+++ src/com/company/ComplexNumber.java
@@ -4,5 +4,28 @@ public class ComplexNumber {
     private double real;
     private double imaginary;
 
-    
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(ComplexNumber number) {
+
+    }
+
+    public void subtract(double real, double imaginary) {
+
+    }
+
+    public void subtract(ComplexNumber number) {
+        
+    }
 }
